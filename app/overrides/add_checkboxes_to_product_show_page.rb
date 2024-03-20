3f620e0957e7eecbbd4c3b906f52d15f2f2451aa
Alejandro Sanchez <>
2024-03-19 21:51:17 -0500

    update version

--- app/overrides/add_checkboxes_to_product_show_page.rb
+++
@@ -1,7 +0,0 @@
-# app/overrides/add_checkboxes_to_cart_form.rb
-Deface::Override.new(
-  virtual_path: "spree/products/_cart_form",
-  name: "add_checkboxes_to_cart_form",
-  insert_before: "[data-hook='inside_product_cart_form']",
-  partial: "spree/products/cart_checkboxes"
-)
