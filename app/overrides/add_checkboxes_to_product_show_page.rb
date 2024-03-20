5175ceb57d83fc581dd64ed028c122a55ed65403
Alejandro Sanchez <>
2024-03-19 21:43:42 -0500

    update version

--- app/overrides/add_checkboxes_to_product_show_page.rb
+++ app/overrides/add_checkboxes_to_product_show_page.rb
@@ -1,6 +1,7 @@
+# app/overrides/add_checkboxes_to_cart_form.rb
 Deface::Override.new(
   virtual_path: "spree/products/_cart_form",
   name: "add_checkboxes_to_cart_form",
-  insert_before: ".add-to-cart",
+  insert_before: "[data-hook='inside_product_cart_form']",
   partial: "spree/products/cart_checkboxes"
 )
