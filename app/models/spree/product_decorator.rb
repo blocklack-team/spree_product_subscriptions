36f38100297296b7c559f8cb40d5df4d36f09791
Alejandro Sanchez <>
2024-03-19 21:40:27 -0500

    update version

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -5,13 +5,13 @@ module Spree
       base.has_many :product_subscription_frequencies, class_name: "Spree::ProductSubscriptionFrequency", dependent: :destroy
       base.has_many :subscription_frequencies, through: :product_subscription_frequencies, dependent: :destroy
 
-      alias_attribute :subscribable, :is_subscribable
+      base.alias_attribute :subscribable, :is_subscribable
 
-      self.whitelisted_ransackable_attributes += %w( is_subscribable )
+      base.whitelisted_ransackable_attributes += %w( is_subscribable )
     
-      scope :subscribable, -> { where(subscribable: true) }
+      base.scope :subscribable, -> { where(subscribable: true) }
     
-      validates :subscription_frequencies, presence: true, if: :subscribable?
+      base.validates :subscription_frequencies, presence: true, if: :subscribable?
     end
 
     ::Spree::Product.prepend self if ::Spree::Product.included_modules.exclude?(self)
