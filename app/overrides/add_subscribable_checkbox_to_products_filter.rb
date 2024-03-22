94c855dd21b1d0f45477322282416f9e9f284742
Alejandro Sanchez <>
2024-03-22 09:54:04 -0500

    update deface files

--- app/overrides/add_subscribable_checkbox_to_products_filter.rb
+++ app/overrides/add_subscribable_checkbox_to_products_filter.rb
@@ -1,4 +1,4 @@
-Deface::Override.new(
+AddSubscribableCheckboxToProductsFilter = Deface::Override.new(
   virtual_path: 'spree/admin/products/index',
   name: 'add_subscribable_filter_to_products',
   insert_bottom: "[data-hook='admin_products_index_search'] .col-md-12",
