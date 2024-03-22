78f1ef5c7d60a70b47b4b4f1c1a4da830f8d71bd
Alejandro Sanchez <>
2024-03-22 11:52:11 -0500

    fix controller

--- app/controllers/spree/api/v2/platform/line_items_controller_decorator.rb
+++ app/controllers/spree/api/v2/platform/line_items_controller_decorator.rb
@@ -5,11 +5,20 @@ module Spree
     module V2
       module Platform
         module LineItemsControllerDecorator
-
           def self.prepended(base)
-            base.line_item_options += [:subscribe, :delivery_number, :subscription_frequency_id]
-          end
+            base.class_eval do
+              def create
+                super # Llama al método create del controlador original
+                # Agrega aquí la lógica adicional si es necesario
 
+                line_item = spree_current_order.line_items.last
+                line_item.subscribe = params[:subscribe]
+                line_item.delivery_number = params[:delivery_number]
+                line_item.subscription_frequency_id = params[:subscription_frequency_id]
+                line_item.save!
+              end
+            end
+          end
         end
       end
     end
@@ -17,3 +26,4 @@ module Spree
 end
 
 ::Spree::Api::V2::Platform::LineItemsController.prepend(Spree::Api::V2::Platform::LineItemsControllerDecorator)
+
