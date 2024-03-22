dbd9c1627b7ced78a61b5cc54b256d5db6c87e4d
Alejandro Sanchez <>
2024-03-22 12:02:57 -0500

    fix controller

--- app/controllers/spree/users_controller_decorator.rb
+++ app/controllers/spree/users_controller_decorator.rb
@@ -1,12 +1,19 @@
-Spree::UsersController.class_eval do
+# app/controllers/spree/users_controller_decorator.rb
 
-  before_action :load_subscriptions, only: :show
-
-  private
-
-    def load_subscriptions
-      @orders = @user.orders.complete.order(completed_at: :desc)
-      @subscriptions = Spree::Subscription.active.order(created_at: :desc).with_parent_orders(@orders)
+module Spree
+  module UsersControllerDecorator
+    def self.prepended(base)
+      base.before_action :load_subscriptions, only: :show
     end
 
+    private
+
+    def load_subscriptions
+      @orders = spree_current_user.orders.complete.order(completed_at: :desc)
+      @subscriptions = Spree::Subscription.active.order(created_at: :desc).with_parent_orders(@orders)
+    end
+  end
 end
+
+::Spree::UsersController.prepend(Spree::UsersControllerDecorator)
+
