d79b99ead863c94f690a64b4ad50a5e9f8479426
Alejandro Sanchez <>
2024-03-22 10:43:40 -0500

    update deface files

--- app/overrides/add_subscribable_to_products.rb
+++ app/overrides/add_subscribable_to_products.rb
@@ -1,4 +1,4 @@
-Deface::Override.new(
+AddSubscribableToProducts = Deface::Override.new(
   virtual_path: 'spree/admin/products/_form',
   name: 'add_subscribable_to_product_edit',
   insert_after: "[data-hook='admin_product_form_description']",
