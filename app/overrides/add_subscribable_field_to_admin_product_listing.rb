59d5791ef1cf025f2565d1adc785db0a3970fce4
Alejandro Sanchez <>
2024-03-22 11:01:19 -0500

    update deface files

--- app/overrides/add_subscribable_field_to_admin_product_listing.rb
+++ app/overrides/add_subscribable_field_to_admin_product_listing.rb
@@ -1,4 +1,4 @@
-AddSubscribableHeaderToAdminProductListing = Deface::Override.new(
+AddSubscribableFieldToAdminProductListing = Deface::Override.new(
   virtual_path: "spree/admin/products/index",
   name: "add_subscribable_header_to_products_listing",
   insert_before: "[data-hook='admin_products_index_headers'] [data-hook='admin_products_index_header_actions']",
@@ -11,3 +11,4 @@ AddSubscribableContentToAdminProductListing = Deface::Override.new(
   insert_before: "[data-hook='admin_products_index_rows'] [data-hook='admin_products_index_row_actions']",
   partial: "spree/admin/products/subscribable_listing_content"
 )
+
