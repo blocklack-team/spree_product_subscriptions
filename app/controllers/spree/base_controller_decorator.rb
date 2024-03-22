5d4650572512eb3d875511a087cd8807fea7d381
Alejandro Sanchez <>
2024-03-22 11:56:53 -0500

    fix controller

--- app/controllers/spree/base_controller_decorator.rb
+++ app/controllers/spree/base_controller_decorator.rb
@@ -1,5 +1,9 @@
-module Spree::BaseControllerDecorator.class_eval do
-  add_flash_types :success, :error
+module Spree::BaseControllerDecorator
+  def self.prepended(base)
+    base.class_eval do
+      add_flash_types :success, :error
+    end
+  end
 end
 
 ::Spree::BaseController.prepend(Spree::BaseControllerDecorator)
