8eea1195cf44d5c19023ea95cc287a876f9416b1
Alejandro Sanchez <>
2024-03-22 09:42:46 -0500

    update deface files

---
+++ app/overrides/add_checkboxes_to_product_show_page.rb
@@ -0,0 +1,8 @@
+# app/overrides/add_subscribable_checkbox_to_products_filter.rb
+
+AddSubscribableCheckboxToProductsFilter = Deface::Override.new(
+  virtual_path: "spree/products/_cart_form",
+  name: "add_checkboxes_to_cart_form",
+  insert_before: ".add-to-cart",
+  partial: "spree/products/cart_checkboxes"
+)
