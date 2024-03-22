c692250034b4d1d2e7f210c10e4dc067046bbe54
Alejandro Sanchez <>
2024-03-22 10:48:51 -0500

    update deface files

--- app/overrides/add_subscription_details_to_order_details.rb
+++ app/overrides/add_subscription_details_to_order_details.rb
@@ -1,18 +1,18 @@
-Deface::Override.new(
+AddSubscriptionDetailsToOrderDetails = Deface::Override.new(
   virtual_path: 'spree/shared/_order_details',
   name: "add_subscription_header_to_order_details",
   insert_bottom: '[data-hook="order_details_line_items_headers"]',
   partial: "spree/orders/cart_subscription_header"
 )
 
-Deface::Override.new(
+AddSubscriptionDetailsToOrderDetails = Deface::Override.new(
   virtual_path: 'spree/shared/_order_details',
   name: "add_subscription_body_to_order_details",
   insert_bottom: "[data-hook='order_details_line_item_row']",
   partial: "spree/shared/subscription_field"
 )
 
-Deface::Override.new(
+AddSubscriptionDetailsToOrderDetails = Deface::Override.new(
   virtual_path: 'spree/shared/_order_details',
   name: "add_subscription_footer_to_order_details",
   insert_bottom: ".total",
