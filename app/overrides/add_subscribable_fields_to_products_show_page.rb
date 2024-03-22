344808460ea68ac2795aa543b6a518b21d552d50
Alejandro Sanchez <>
2024-03-22 10:42:07 -0500

    update deface files

--- app/overrides/add_subscribable_fields_to_products_show_page.rb
+++ app/overrides/add_subscribable_fields_to_products_show_page.rb
@@ -1,4 +1,4 @@
-Deface::Override.new(
+AddSubscribableFieldsToProductsShowPage = Deface::Override.new(
   virtual_path: "spree/products/_cart_form",
   name: "add_subscribable_fields_to_products_show",
   insert_after: ".add-to-cart",
