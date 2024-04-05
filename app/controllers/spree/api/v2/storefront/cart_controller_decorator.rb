f56066d7054cf86ca4a920ef4448527e024d72f6
Alejandro Sanchez <>
2024-04-04 23:31:58 -0500

    update product decorator

--- app/controllers/spree/api/v2/storefront/cart_controller_decorator.rb
+++ app/controllers/spree/api/v2/storefront/cart_controller_decorator.rb
@@ -1,27 +1,34 @@
 module Spree
-	module Api
-		module V2
-			module Storefront
-				module CartControllerDecorator
+  module Api
+    module V2
+      module Storefront
+        module CartControllerDecorator
           def self.prepended(base)
+            base.include InstanceMethods
+          end
+
+          module InstanceMethods
             def add_item
-							result = add_item_service.call(
-								order: spree_current_order,
-								variant: @variant,
-								quantity: add_item_params[:quantity],
-								public_metadata: add_item_params[:public_metadata],
-								private_metadata: add_item_params[:private_metadata],
-								options: add_item_params[:options],
-								subscribe: 1
-							)
-	
-							render_order(result)
+              spree_authorize! :update, spree_current_order, order_token
+              spree_authorize! :show, @variant
+
+              result = add_item_service.call(
+                order: spree_current_order,
+                variant: @variant,
+                quantity: add_item_params[:quantity],
+                public_metadata: add_item_params[:public_metadata],
+                private_metadata: add_item_params[:private_metadata],
+                options: add_item_params[:options],
+                subscribe: 1
+              )
+
+              render_order(result)
             end
           end
-				end
-			end
-		end
-	end
+        end
+      end
+    end
+  end
 end
 
 ::Spree::Api::V2::Storefront::CartController.prepend(Spree::Api::V2::Storefront::CartControllerDecorator)
