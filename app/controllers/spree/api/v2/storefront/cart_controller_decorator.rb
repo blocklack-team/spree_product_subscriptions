897c8029498eb021b1bbfd92934ecafcfe23dafa
Alejandro Sanchez <>
2024-04-05 00:25:17 -0500

    update product decorator

--- app/controllers/spree/api/v2/storefront/cart_controller_decorator.rb
+++ app/controllers/spree/api/v2/storefront/cart_controller_decorator.rb
@@ -4,26 +4,31 @@ module Spree
       module Storefront
         module CartControllerDecorator
           def self.prepended(base)
-            def base.add_item
-              p 'cart Items decorator'
-              p 'cart Items decorator'
-              p 'cart Items decorator'
-              p 'cart Items decorator'
-              p 'cart Items decorator'
-  
-              result = add_item_service.call(
-                order: spree_current_order,
-                variant: @variant,
-                quantity: add_item_params[:quantity],
-                public_metadata: add_item_params[:public_metadata],
-                private_metadata: add_item_params[:private_metadata],
-                options: add_item_params[:options],
-                subscribe: 1
-              )
-  
-              p result
-  
-              render_order(result)
+            base.class_eval do
+              def add_item_with_subscription
+                p 'cart Items decorator'
+                p 'cart Items decorator'
+                p 'cart Items decorator'
+                p 'cart Items decorator'
+                p 'cart Items decorator'
+
+                result = add_item_service.call(
+                  order: spree_current_order,
+                  variant: @variant,
+                  quantity: add_item_params[:quantity],
+                  public_metadata: add_item_params[:public_metadata],
+                  private_metadata: add_item_params[:private_metadata],
+                  options: add_item_params[:options],
+                  subscribe: 1
+                )
+
+                p result
+
+                render_order(result)
+              end
+
+              alias_method :add_item_without_subscription, :add_item
+              alias_method :add_item, :add_item_with_subscription
             end
           end
         end
