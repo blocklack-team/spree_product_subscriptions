95ae28025995b7d84318f1b7723e2fced7d95443
Alejandro Sanchez <>
2024-04-05 01:00:11 -0500

    update product decorator

--- app/controllers/spree/api/v2/storefront/cart_controller_decorator.rb
+++ app/controllers/spree/api/v2/storefront/cart_controller_decorator.rb
@@ -5,30 +5,7 @@ module Spree
         module CartControllerDecorator
           def self.prepended(base)
             base.class_eval do
-              def add_item
-                p 'cart Items decorator'
-                p 'cart Items decorator'
-                p 'cart Items decorator'
-                p 'cart Items decorator'
-                p 'cart Items decorator'
-                
-                spree_authorize! :update, spree_current_order, order_token
-                spree_authorize! :show, @variant
 
-                result = add_item_service.call(
-                  order: spree_current_order,
-                  variant: @variant,
-                  quantity: add_item_params[:quantity],
-                  public_metadata: add_item_params[:public_metadata],
-                  private_metadata: add_item_params[:private_metadata],
-                  options: add_item_params[:options],
-                  subscribe: 1
-                )
-
-                p result
-
-                render_order(result)
-              end
             end
           end
         end
