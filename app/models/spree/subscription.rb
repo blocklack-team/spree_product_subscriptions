79b77bc862142aaf079bd3ae544be0125c5ab367
Apple <apple@Apples-MacBook-Pro.local>
2024-06-14 12:53:44 -0500

    update bundle suscription fix price

--- app/models/spree/subscription.rb
+++ app/models/spree/subscription.rb
@@ -134,7 +134,7 @@ module Spree
 
       def update_price
         if valid_variant?
-          self.price = variant.price
+          self.price = variant.prices[0].amount
         else
           self.errors.add(:variant_id, :does_not_belong_to_product)
         end
