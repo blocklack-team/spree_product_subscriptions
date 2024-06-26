92a6cdbb0322939c9250ca70ee56839d9d2dcdae
Alejandro Sanchez <apple@Apples-MacBook-Pro.local>
2024-06-26 01:14:48 -0500

    update subscription

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -30,8 +30,8 @@ module Spree
         subscription.update(
           source: payments.from_credit_card.first.source,
           enabled: true,
-          ship_address: user.present ? user.ship_address.try(:clone) : ship_address.clone,
-          bill_address: user.present ? user.bill_address.try(:clone) : bill_address.clone
+          ship_address: user.present? ? user.ship_address.try(:clone) : ship_address.clone,
+          bill_address: user.present? ? user.bill_address.try(:clone) : bill_address.clone
         )
       end
       p '#Finish actualizando o creando la subscription'
@@ -40,7 +40,6 @@ module Spree
     end
 
     def any_disabled_subscription?
-      p subscriptions.disabled
       subscriptions.disabled.any?
     end
 
